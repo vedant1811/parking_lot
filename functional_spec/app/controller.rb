e2833f28e6f52e7e80333708d652ea00848d6aaf
Vedant Agarwala <vedant.kota@gmail.com>
2018-08-22 15:37:10 +0530

    Implement Controller to read lines and execute commands

--- functional_spec/app/controller.rb
+++ functional_spec/app/controller.rb
@@ -1,13 +1,48 @@
-$LOAD_PATH.unshift File.expand_path("../app", __FILE__)
+$LOAD_PATH.unshift File.expand_path("../lib", __FILE__)
 
-puts "loading file: controller.rb"
+require 'parking_lot'
+require 'models/car'
+require 'models/parking_slot'
 
 module Controller
-  def create_parking_lot(size)
+  # read from file or STDIN
+  def self.included(base)
+    while line = gets
+      break if line.chomp.empty?
+      puts line
+      words = line.split
+      public_send *words
+    end
+  end
 
+  def create_parking_lot(size)
+    @parking_lot = ParkingLot.new size.to_i
   end
 
   def park(number, colour)
+    @parking_lot.park number, colour
+  end
 
+  def leave(slot)
+    @parking_lot.leave slot
+  end
+
+  def status
+    # TODO: do
+  end
+
+  def registration_numbers_for_cars_with_colour(colour)
+    Car.registration_numbers_for_cars_with_colour colour
+  end
+
+  def slot_numbers_for_cars_with_colour(colour)
+    ParkingSlot.slot_numbers_for_cars_with_colour(colour)
+  end
+
+  def slot_number_for_registration_number(number)
+    ParkingSlot.slot_number_for_registration_number(number)
   end
 end
+
+# run the program
+include Controller
